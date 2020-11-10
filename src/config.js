6f99922feffc85fe6eda609d564fbf75a32203a8
Jason Stankevich <jason.stankevich@gmail.com>
2020-11-09 22:26:17 -0500

    deployment changes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
+  REACT_APP_API_BASE:
+    process.env.REACT_APP_API_BASE || 'http://localhost:8000/api',
+  TOKEN_KEY: process.env.TOKEN_KEY || 'spaced-repetition-jwt-secret',
 };
