81ba0e3fdda8ecdc9c5772ffd78c43bf0d9b00e9
Jason Stankevich <jason.stankevich@gmail.com>
2020-11-09 22:39:10 -0500

    fixed deployment issues

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  REACT_APP_API_BASE:
-    process.env.REACT_APP_API_BASE || 'http://localhost:8000/api',
+  API_ENDPOINT:
+    process.env.API_ENDPOINT || 'http://localhost:8000/api',
   TOKEN_KEY: process.env.TOKEN_KEY || 'spaced-repetition-jwt-secret',
 };
